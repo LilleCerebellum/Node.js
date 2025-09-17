72ee420a8e19090c02dfe729536a0c589485638b
LilleCerebellum <160247250+LilleCerebellum@users.noreply.github.com>
2025-09-17 23:08:41 +0200

    Update, added button and pages test

--- API/server.js
+++ API/server.js
@@ -7,14 +7,26 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
     try {
-        const htmlPath = path.join(__dirname, '..', 'index.html');
+        const htmlPath = path.join(__dirname, '..', 'public/index.html');
         const html = await readFile(htmlPath, 'utf8');
         res.send(html);
     } catch (err) {
         res.status(500).send('Error loading HTML file.');
     }
 });
-    
+
+app.get('/cv', async (req, res) => {
+    try {
+        const htmlPath = path.join(__dirname, '..', 'public/cv.html');
+        const html = await readFile(htmlPath, 'utf8');
+        res.send(html);
+    } catch (err) {
+        res.status(500).send('Error loading HTML file.');
+    }
+});
+
+app.use(express.static(path.join(__dirname, '..', 'Public')));
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ public/scripts.js
@@ -0,0 +1,13 @@
+function ToCV() {
+    window.location.href = '/cv.html';
+}
+function ToAlbum() {
+    window.location.href = 'album.html';
+}
+function ToFiles() {
+    window.location.href = 'files.html';
+}
+function ToLibrary() {
+    window.location.href = 'library.html';
+}
+
