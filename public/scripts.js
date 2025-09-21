ec595ff25bb1af5468a02a7a00211c7b255c96da
LilleCerebellum <Soellingvraa72@gmail.com>
2025-09-21 21:39:17 +0200

    CV page updated to finish

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,27 @@
+function ToHome() {
+    window.location.href = 'index.html';
+}
+
 function ToCV() {
     window.location.href = 'cv.html';
 }
+
 function ToAlbum() {
     window.location.href = 'album.html';
 }
+
 function ToFiles() {
     window.location.href = 'files.html';
 }
+
 function ToLibrary() {
     window.location.href = 'library.html';
 }
+
+function Notes() {
+    window.location.href = 'notes.html';
+}
+
 function ToManifesto() {
     window.location.href = 'Manifesto.html';
 }
